3c0e239f190b38d1a451b2ad0d130171e3508dce
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-11-29 08:00:55 +0900

    カメラをプレイヤーに追従させる

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour {
     private Animator _animator;
     private Rigidbody2D _rigidbody2D;
 
+    [SerializeField]
+    private GameObject _camera;
+
     // Use this for initialization
     void Start() {
         _animator = GetComponent<Animator>();
@@ -29,11 +32,36 @@ public class Player : MonoBehaviour {
             transform.localScale = temp;
             // 走る
             _animator.SetBool("run", true);
+            // カメラをプレイヤーに追従させる
+            moveCamera();
         } else {
             // 横移動の速度を0にしてピタッと止まるようにする
             _rigidbody2D.velocity = new Vector2(0, _rigidbody2D.velocity.y);
             // 待機
             _animator.SetBool("run", false);
         }
+    }
+
+    private void moveCamera() {
+        const int THRESHOLD = 4;
+
+        // 画面中央から左に少し移動した位置をユニティちゃんが超えたら
+        if (transform.position.x > _camera.transform.position.x - THRESHOLD) {
+            // カメラの位置を取得
+            Vector3 cameraPosition = _camera.transform.position;
+            // Playerの位置から右に少し移動した位置を画面中央にする
+            cameraPosition.x = transform.position.x + THRESHOLD;
+            _camera.transform.position = cameraPosition;
+        }
+
+        //カメラ表示領域の左下をワールド座標に変換
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        //カメラ表示領域の右上をワールド座標に変換
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        Vector2 playerPosition = transform.position;
+        // Playerのx座標の移動範囲をClampメソッドで制限
+        playerPosition.x = Mathf.Clamp(playerPosition.x, min.x + 0.5f, max.x);
+        transform.position = playerPosition;
     }
 }
