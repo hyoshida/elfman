6a987fecba17a817ab7f79948eb0425b64708bcd
YOSHIDA Hiroki <hyoshida@appirits.com>
2017-01-03 17:02:35 +0900

    ボスを倒したらとりあえずゲームオーバーにする

--- Assets/Scripts/Platforms/Gate.cs
+++ Assets/Scripts/Platforms/Gate.cs
@@ -5,8 +5,14 @@ public class Gate : MonoBehaviour {
     [SerializeField]
     GameObject _goal;
 
+    [SerializeField]
+    GameObject _bossSpawner;
+
     GameObject _player;
     bool _closed;
+
+    void Start() {
+    }
 
     void OnTriggerEnter2D(Collider2D collider) {
         if (collider.tag != "Player") {
@@ -29,6 +35,10 @@ public class Gate : MonoBehaviour {
         _player.transform.position = _goal.transform.position;
 
         yield return new WaitForSeconds(0.5f);
+
+        _bossSpawner.GetComponent<BossSpawner>().Spawn();
+
+        yield return new WaitForSeconds(1f);
 
         GameManager.Instance.gameState = GameState.Playing;
         GameManager.Instance.battleMode = BattleMode.Boss;
---
+++ Assets/Scripts/Sprites/BossSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class BossSpawner : MonoBehaviour {
+    [SerializeField]
+    GameObject _enemyPrefab;
+
+    GameObject _enemy;
+    bool _spawned;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (_spawned) {
+            watchEnemy();
+        }
+    }
+
+    public void Spawn() {
+        if (_spawned) {
+            return;
+        }
+        _spawned = true;
+
+        _enemy = Instantiate(_enemyPrefab, transform.position, transform.rotation);
+        _enemy.transform.SetParent(transform.parent);
+    }
+
+    void watchEnemy() {
+        if (_enemy) {
+            return;
+        }
+
+        // TODO: この辺の作りかなり雑
+        var stageScene = ApplicationScene.Instance as Stage.Scene;
+        if (stageScene) {
+            stageScene.GameOver();
+        }
+    }
+}
