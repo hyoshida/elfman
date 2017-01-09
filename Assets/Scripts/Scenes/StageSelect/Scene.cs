1e92cd654a91d1e281199d8652e3f4a7df79d406
YOSHIDA Hiroki <hyoshida@appirits.com>
2017-01-09 23:24:02 +0900

    ステージ選択シーンを作った
    Refs #28

---
+++ Assets/Scripts/Scenes/StageSelect/Scene.cs
@@ -0,0 +1,7 @@
+﻿namespace StageSelect {
+    public class Scene : ApplicationScene {
+        public void onClicktStage(int stageCode) {
+            GameManager.Instance.GotoStage((uint)stageCode);
+        }
+    }
+}
