348636d9c26e99ab8726b70d07577a0d1f3a4713
YOSHIDA Hiroki <hyoshida@appirits.com>
2017-02-02 00:42:07 +0900

    近距離攻撃が当たったらゲーム速度をスローにする

---
+++ Assets/Scripts/Sprites/Player/PlayerSword.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerSword : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.tag == "Enemy") {
+            // NOTE: 攻撃ヒット中はゲーム進行をスローにしたい
+            Time.timeScale = 0.5f;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider) {
+        Time.timeScale = 1;
+    }
+
+    void OnDisable() {
+        Time.timeScale = 1;
+    }
+}
