2eb2604c750918f127f55f0be279a8c282eab134
YOSHIDA Hiroki <hyoshida@appirits.com>
2017-02-03 01:03:44 +0900

    小ジャンプを実装した

--- Assets/Scripts/Sprites/Player/PlayerJumpingAction.cs
+++ Assets/Scripts/Sprites/Player/PlayerJumpingAction.cs
@@ -2,10 +2,13 @@
 using Assets.Scripts.Extensions;
 
 public class PlayerJumpingAction {
+    public const float TIME_FOR_FULL_JUMP = 0.25f;
+
     Player _player;
     int _jumpPower;
     Rigidbody2D _rigidbody2D;
     Animator _animator;
+    float _timeHeld;
 
     public PlayerJumpingAction(Player player, int jumpPower) {
         _player = player;
@@ -16,17 +19,37 @@ public class PlayerJumpingAction {
 
     public void Update() {
         if (Input.GetButtonDown("Jump")) {
-            // 着地してたとき
-            if (_player.IsGrounded && _animator.IsPlaying("waiting", "running", "running-attack1", "dashing", "jumping5")) {
-                _player.IsGrounded = false;
-
-                // runアニメーションを止めて、jumpアニメーションを実行
-                _animator.SetBool("isRunning", false);
-                _animator.SetTrigger("jump");
-
-                // AddForceにて上方向へ力を加える
-                _rigidbody2D.AddForce(Vector2.up * _jumpPower);
-            }
+            _timeHeld = 0f;
+            Jump();
         }
+
+        if (Input.GetButton("Jump")) {
+            _timeHeld += Time.deltaTime;
+        }
+
+        if ((Input.GetButtonUp("Jump") || _timeHeld >= TIME_FOR_FULL_JUMP) && !_player.IsGrounded) {
+            float ratio = Mathf.SmoothStep(1f, 0f, _timeHeld / TIME_FOR_FULL_JUMP);
+            _rigidbody2D.AddRelativeForce(Vector2.up * _jumpPower * ratio * -0.75f);
+            _timeHeld = TIME_FOR_FULL_JUMP;
+        }
+    }
+
+    void Jump() {
+        if (!_player.IsGrounded) {
+            return;
+        }
+
+        if (!_animator.IsPlaying("waiting", "running", "running-attack1", "dashing", "jumping5")) {
+            return;
+        }
+
+        _player.IsGrounded = false;
+
+        // runアニメーションを止めて、jumpアニメーションを実行
+        _animator.SetBool("isRunning", false);
+        _animator.SetTrigger("jump");
+
+        // AddForceにて上方向へ力を加える
+        _rigidbody2D.AddForce(Vector2.up * _jumpPower);
     }
 }
