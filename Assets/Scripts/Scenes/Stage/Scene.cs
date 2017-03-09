f1f5c01ad089dbfad91b990400e90aef4d83ee57
YOSHIDA Hiroki <hyoshida@appirits.com>
2017-03-09 22:35:24 +0900

    かけあい後に操作可能になる

--- Assets/Scripts/Scenes/Stage/Scene.cs
+++ Assets/Scripts/Scenes/Stage/Scene.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 namespace Stage {
     enum State {
+        Scenario,
         Playing,
         GameOver,
         GameClear,
@@ -15,14 +16,32 @@ namespace Stage {
         GameObject _player;
 
         [SerializeField]
+        GameObject _hud;
+
+        [SerializeField]
         GameObject _gameOverLabel;
 
         [SerializeField]
         GameObject _gameClearLabel;
 
+        [SerializeField]
+        GameObject _scenarioViewer;
+
         Player _playerInstance;
         bool _gameOvered;
         State _state;
+
+        public void GameStart() {
+            if (_state == State.Playing) {
+                return;
+            }
+            _state = State.Playing;
+
+            _hud.SetActive(true);
+            _scenarioViewer.SetActive(false);
+
+            StartCoroutine(PlayAfterCurrentFrame());
+        }
 
         public void GameOver() {
             if (_state == State.GameOver) {
@@ -43,13 +62,21 @@ namespace Stage {
 
         // Use this for initialization
         void Start() {
-            _state = State.Playing;
+            _state = State.Scenario;
             _playerInstance = _player.GetComponent<Player>();
+
+            _hud.SetActive(false);
+            _scenarioViewer.SetActive(true);
+
+            GameManager.Instance.gameState = GameState.Pause;
         }
 
         // Update is called once per frame
         void Update() {
             switch (_state) {
+                case State.Scenario:
+                    UpdateForScenarioState();
+                    break;
                 case State.Playing:
                     UpdateForPlayingState();
                     break;
@@ -59,6 +86,14 @@ namespace Stage {
                 case State.GameClear:
                     UpdateForGameClearState();
                     break;
+            }
+        }
+
+        void UpdateForScenarioState() {
+            // TODO: マスタを参照する
+
+            if (Input.GetButtonDown("Fire1")) {
+                GameStart();
             }
         }
 
@@ -78,6 +113,12 @@ namespace Stage {
             if (Input.GetButtonDown("Fire1")) {
                 GameManager.Instance.GotoStageSelect();
             }
+        }
+
+        // NOTE: 決定キー後に攻撃モーションをとるのを避けるために１フレームだけポーズ解除をズラす
+        IEnumerator PlayAfterCurrentFrame() {
+            yield return new WaitForEndOfFrame();
+            GameManager.Instance.gameState = GameState.Playing;
         }
     }
 }
