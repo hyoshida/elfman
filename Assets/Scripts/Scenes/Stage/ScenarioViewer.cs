fbe69b8321ff8f31b50ca6142841ea3aa9917908
YOSHIDA Hiroki <hyoshida@appirits.com>
2017-04-04 23:55:24 +0900

    マスターに設定したテキストを画面に表示する

--- Assets/Scripts/Scenes/Stage/ScenarioViewer.cs
+++ Assets/Scripts/Scenes/Stage/ScenarioViewer.cs
@@ -1,34 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ScenarioViewer : MonoBehaviour {
+    [SerializeField]
+    Text _text;
+
     SceneMaster _currentSceneMaster;
     int _textIndex;
 
-    void Finish() {
-        gameObject.SetActive(false);
-    }
-
     void OnEnable() {
         _textIndex = 0;
+
         // TODO: ちゃんとエンディングを再生できるようにする
         bool openning = true;
         _currentSceneMaster = Master.Instance.FindSceneMasterBy(GameManager.Instance.CurrentStageCode, openning);
+
+        Next();
     }
 
-    // Use this for initialization
-    void Start() {
-    }
-
-    // Update is called once per frame
     void Update() {
         if (_currentSceneMaster == null) {
             Finish();
             return;
         }
 
-        if (!Input.GetButtonDown("Fire1")) {
+        if (Input.GetButtonDown("Fire1")) {
+            Next();
+        }
+    }
+
+    void Next() {
+        if (_currentSceneMaster == null) {
+            Finish();
             return;
         }
 
@@ -39,9 +42,12 @@ public class ScenarioViewer : MonoBehaviour {
         }
 
         string text = _currentSceneMaster.texts[_textIndex];
-        // TODO: 画面に表示する
-        Debug.Log(text);
+        _text.text = text;
 
         _textIndex++;
+    }
+
+    void Finish() {
+        gameObject.SetActive(false);
     }
 }
