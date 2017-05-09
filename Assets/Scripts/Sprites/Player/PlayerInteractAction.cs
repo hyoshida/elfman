5aa4381baf55973c40ff2110d81cee154b6503a4
YOSHIDA Hiroki <hyoshida@appirits.com>
2017-05-09 23:31:08 +0900

    上方向の入力でインタラクトする

---
+++ Assets/Scripts/Sprites/Player/PlayerInteractAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ↑ボタンを押すとプレイヤーの付近にあるものにインタラクトする
+public class PlayerInteractAction : MonoBehaviour {
+    void Update() {
+        float axis = Input.GetAxisRaw("Vertical");
+        int direction = (axis == 0) ? 0 : ((axis > 0) ? 1 : -1);
+        if (direction == 1) {
+            Interact();
+        }
+    }
+
+    void Interact() {
+        // TODO: 実装する
+        Debug.Log("Interact!!");
+    }
+}
