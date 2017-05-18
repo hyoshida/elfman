6f964fdd616403341b4364d18b86e85072398b1f
YOSHIDA Hiroki <hyoshida@appirits.com>
2017-05-18 23:00:48 +0900

    転送メニューを実装した

---
+++ Assets/Scripts/UI/WarpMenu.cs
@@ -0,0 +1,33 @@
+﻿using Stage;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarpMenu : MonoBehaviour {
+    [SerializeField]
+    Button _defaultMenuItem;
+
+    Scene _scene;
+
+    Scene scene {
+        get {
+            return _scene = _scene ? _scene : (Scene)ApplicationScene.Instance;
+        }
+    }
+
+    public void OnOpen() {
+        scene.RevertFromMenuMode();
+
+        gameObject.SetActive(true);
+        scene.ChangeToMenuMode(() => gameObject.SetActive(false));
+
+        _defaultMenuItem.Select();
+    }
+
+    public void OnSelectStage(string stageCode) {
+        GameManager.Instance.GotoStage(uint.Parse(stageCode));
+    }
+
+    public void OnCancel() {
+        scene.RevertFromMenuMode();
+    }
+}
