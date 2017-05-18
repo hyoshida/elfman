cf79c89903f6d42d87ae77d1f1b0de3707b25098
YOSHIDA Hiroki <hyoshida@appirits.com>
2017-05-18 22:36:20 +0900

    メニューをコントローラで選択できるようにした

--- Assets/Scripts/Platforms/InteractiveObject/Elevator.cs
+++ Assets/Scripts/Platforms/InteractiveObject/Elevator.cs
@@ -1,12 +1,17 @@
 ﻿using Stage;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Elevator : InteractiveObject {
     [SerializeField]
     GameObject _elevatorMenu;
 
+    [SerializeField]
+    Button _defaultMenuItem;
+
     override public void Interact() {
         _elevatorMenu.SetActive(true);
+        _defaultMenuItem.Select();
 
         var scene = (Scene)ApplicationScene.Instance;
         scene.ChangeToMenuMode(() => _elevatorMenu.SetActive(false));
