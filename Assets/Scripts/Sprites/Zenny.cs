8af499460b930e5ae5a19493f842c738375db0da
YOSHIDA Hiroki <hyoshida@appirits.com>
2018-02-07 00:27:53 +0900

    お金は一定時間経つとプレイヤー目指して移動する

--- Assets/Scripts/Sprites/Zenny.cs
+++ Assets/Scripts/Sprites/Zenny.cs
@@ -1,17 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Stage;
 using UnityEngine;
 
 public class Zenny : MonoBehaviour {
+    public const float FOLLOWING_SPEED = 50f;
+
     [SerializeField]
     int _amount;
 
+    GameObject _player;
+    float _deltaTime;
+    Rigidbody2D _rigidbody2D;
+    bool _followed;
+
     // Use this for initialization
     void Start() {
+        var currentScene = Camera.main.GetComponent<Scene>();
+        _player = currentScene.Player;
+
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
+        _deltaTime += Time.deltaTime;
+        if (_deltaTime >= 2.0f) {
+            if (!_followed) {
+                _rigidbody2D.isKinematic = true;
+            }
+            _followed = true;
+
+            FollowingPlayer();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
@@ -25,5 +44,11 @@ public class Zenny : MonoBehaviour {
         if (player != null) {
             player.TakeZenny(_amount);
         }
+    }
+
+    void FollowingPlayer() {
+        transform.LookAt(_player.transform);
+        var offset = transform.forward * FOLLOWING_SPEED * Time.deltaTime;
+        transform.position += new Vector3(offset.x, offset.y);
     }
 }
