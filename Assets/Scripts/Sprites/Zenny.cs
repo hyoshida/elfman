b90111d46f873be160957a57b567858403af405a
YOSHIDA Hiroki <hyoshida@appirits.com>
2018-02-15 22:29:38 +0900

    お金が変にプレイヤーとぶつからないようにした

--- Assets/Scripts/Sprites/Zenny.cs
+++ Assets/Scripts/Sprites/Zenny.cs
@@ -10,7 +10,14 @@ public class Zenny : MonoBehaviour {
     GameObject _player;
     float _deltaTime;
     Rigidbody2D _rigidbody2D;
+    BoxCollider2D _boxCollider2D;
     bool _followed;
+    int _defaultLayer;
+
+    void Awake() {
+        _defaultLayer = gameObject.layer;
+        gameObject.layer = LayerMask.NameToLayer("Invincible");
+    }
 
     // Use this for initialization
     void Start() {
@@ -18,6 +25,7 @@ public class Zenny : MonoBehaviour {
         _player = currentScene.Player;
 
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -26,6 +34,8 @@ public class Zenny : MonoBehaviour {
         if (_deltaTime >= 2.0f) {
             if (!_followed) {
                 _rigidbody2D.isKinematic = true;
+                _boxCollider2D.isTrigger = true;
+                gameObject.layer = _defaultLayer;
             }
             _followed = true;
 
